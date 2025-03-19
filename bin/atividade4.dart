cd29582a900b267aa75b59cb47bfdef19e2c2a53
João Teixeira <138622216+JTeixeiraz@users.noreply.github.com>
2025-03-18 21:18:12 -0300

    Correção do codigo

--- bin/atividade4.dart
+++ bin/atividade4.dart
@@ -1,18 +1,39 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: !');
+  
 }
 
 class Produto{
-  late final String? nome;
-  late final double? preco;
-  late final int? quantidadeEmEstoque;
-  late String descricao;
+  final String? nome;
+  final double? preco;
+  late int? _quantidadeEmEstoque;
+  late String? _descricao;
 
-  Produto(this.nome, this.preco, this.quantidadeEmEstoque);
+  Produto(this.nome, this.preco);
 
-  void vender(int quantidade){
+  get getDescricao => _descricao;
+  set setDescricao(String? descricao){
+    _descricao = descricao;
+  }
+
+  get getQuantidade => _quantidadeEmEstoque;
+  set setQuantidade(int? quant){
+    _quantidadeEmEstoque = quant;
+  }
+
+  void vender(){
+    print('Quanto deseja vender/reduzir do estoque');
+    String? valVenda = stdin.readLineSync()??"";
+    int valIntVenda = int.parse(valVenda);
+    if(valIntVenda > getQuantidade){
+      print('valor maior que o estoque atual');
+    }else{
+      setQuantidade = getQuantidade - valIntVenda;
+    }
+  }
+
+  void reporEstoque(){
     
   }
 }
