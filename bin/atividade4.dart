cab7eec5ede2632cba7e7c9cb6399df8b5786928
João Teixeira <138622216+JTeixeiraz@users.noreply.github.com>
2025-03-21 17:02:13 -0300

    Update - Adicionando novos métodos e inicio de nova classe + outra logica

--- bin/atividade4.dart
+++ bin/atividade4.dart
@@ -1,7 +1,36 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  
+
+
+}
+class Estoque{
+  List<Map<String, dynamic>>estoque = [];
+
+  void adicionarProduto(){
+    print('digite o nome do produto que deseja adicionar:');
+    String nome = stdin.readLineSync()??"";
+    print('Digite o preco do produto:');
+    String preco = stdin.readLineSync()??"";
+    double? precoval = double.tryParse(preco);
+    Produto produto = Produto(nome, precoval);
+    print('Deseja adicionar uma descrição no produto?');
+    print('y/n');
+    String? escolha = stdin.readLineSync()?.toUpperCase()??"";
+    switch (escolha) {
+      case "Y":
+            
+        break;
+      default:
+      print('Adicione a quantidade desse produto em estoque:');
+      String? quant = stdin.readLineSync()??"";
+      int? quantVal = int.tryParse(quant);
+      produto.setQuantidade = quantVal;
+      print('Produto ${produto.nome} adicionado com o preço de \n${produto.preco} \nValor do produto em estoque: \n${produto.getQuantidade}');
+      break;
+    }
+  }
+
 }
 
 class Produto{
@@ -9,6 +38,7 @@ class Produto{
   final double? preco;
   late int? _quantidadeEmEstoque;
   late String? _descricao;
+
 
   Produto(this.nome, this.preco);
 
@@ -34,6 +64,30 @@ class Produto{
   }
 
   void reporEstoque(){
-    
+    print('Oque deseja fazer?: \n 1-Atualizar estoque inteiro \n 2-Adicionar ao estoque atual \n 3-diminuir estoque');
+    String escolha = stdin.readLineSync()??"";
+    switch (escolha) {
+      case "1":
+        print('Digite o novo valor do estoque: ');
+        String novo = stdin.readLineSync()??"";
+        int? novoValor = int.tryParse(novo);
+        setQuantidade = novoValor;
+        print('Valor de estoque alterado para: $getQuantidade');
+        break;
+      case "2":
+        print('Adicione o valor que deseja adicionar ao estoque atual: ');
+        String novo = stdin.readLineSync()??"";
+        int? novoValor = int.tryParse(novo);
+        setQuantidade = getQuantidade + novoValor;
+        print('Valor do estoque alterado para: $getQuantidade');
+      break;
+      case "3":
+        print('Digite o valor que deseja diminuir do estoque:');
+        String menos = stdin.readLineSync()??"";
+        int? menosValor = int.tryParse(menos);
+        setQuantidade = getQuantidade - menosValor;
+      break;
+      default:
+    }
   }
 }
